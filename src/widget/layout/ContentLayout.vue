55161cd3244ff8cd8f24ce1bda757316f7659263
bae woong <109705692+oobg@users.noreply.github.com>
2024-12-28 01:27:10 +0900

    feat: add ContentLayout

---
+++ src/widget/layout/ContentLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <default-header />
+  <main>
+    <router-view />
+    <default-footer />
+  </main>
+</template>
+
+<script setup lang="ts">
+import DefaultHeader from "@/widget/header/DefaultHeader.vue";
+import DefaultFooter from "@/widget/footer/DefaultFooter.vue";
+</script>
+
+<style scoped>
+main > router-view {
+  flex: 1;
+  overflow: auto;
+}
+
+main > footer {
+  flex-shrink: 0;
+}
+</style>
