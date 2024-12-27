6906763c26650bfd63009620eeaf16a6b142e46c
bae woong <109705692+oobg@users.noreply.github.com>
2024-12-28 00:14:05 +0900

    feat: add layout

---
+++ src/widget/footer/DefaultFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>
+    <h1>Default Footer</h1>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/widget/layout/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <default-header />
+  <main>
+    <router-view />
+  </main>
+  <default-footer />
+</template>
+
+<script setup lang="ts">
+import DefaultHeader from "@/widget/header/DefaultHeader.vue";
+import DefaultFooter from "@/widget/footer/DefaultFooter.vue";
+</script>
+
+<style scoped></style>
