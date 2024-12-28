a9e7031b032573cafe8925237919c1cccd3f6d8d
bae woong <109705692+oobg@users.noreply.github.com>
2024-12-28 22:10:21 +0900

    feat: add logo, flex spacer, and icons to header for improved layout

--- src/widget/header/DefaultHeader.vue
+++ src/widget/header/DefaultHeader.vue
@@ -1,9 +1,41 @@
 <template>
   <header>
-    <h1>Default Header</h1>
+    <img
+      src="@/shared/img/logo.svg"
+      alt="Vue logo"
+      class="logo"
+      width="30"
+      height="30"
+    />
+    <h1>Vuyah</h1>
+    <flex-spacer />
+    <icon-community />
+    <icon-documentation />
+    <icon-ecosystem />
+    <icon-support />
+    <icon-tooling />
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+import FlexSpacer from "@/shared/ui/FlexSpacer.vue";
 
-<style scoped></style>
+const IconCommunity = defineAsyncComponent(() => import("@/shared/img/main-icon/IconCommunity.vue"));
+const IconDocumentation = defineAsyncComponent(() => import("@/shared/img/main-icon/IconDocumentation.vue"));
+const IconEcosystem = defineAsyncComponent(() => import("@/shared/img/main-icon/IconEcosystem.vue"));
+const IconSupport = defineAsyncComponent(() => import("@/shared/img/main-icon/IconSupport.vue"));
+const IconTooling = defineAsyncComponent(() => import("@/shared/img/main-icon/IconTooling.vue"));
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+
+  padding: 0 0.5rem;
+}
+</style>
