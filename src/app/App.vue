23608336fa1435533f9cce3a0bd7cddaaf360dc7
bae woong <109705692+oobg@users.noreply.github.com>
2024-12-30 00:10:16 +0900

    fix: prevent layout flickering by dynamically loading DefaultLayout

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,11 +3,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, defineAsyncComponent } from "vue";
 import { useRoute } from "vue-router";
-import DefaultLayout from "@/widget/layout/DefaultLayout.vue";
 
 const route = useRoute();
+const DefaultLayout = defineAsyncComponent(() => import("@/widget/layout/DefaultLayout.vue"));
 const layout = computed(() => route.meta.layout || DefaultLayout);
 </script>
 
